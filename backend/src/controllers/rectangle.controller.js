92ecfb05778c89c016a7af8f59bc0e00e7c5adbb
Gaurav Singh <gauravsingh.info@gmail.com>
2023-11-30 15:38:09 +0530

    fixes

--- backend/src/controllers/rectangle.controller.js
+++ backend/src/controllers/rectangle.controller.js
@@ -39,33 +39,75 @@ const getRectanglebyTemplate = async (req, res) => {
     }
 }
 
+function isUUID(str) {
+    const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+    return uuidPattern.test(str);
+  }
+
+// const createUpdateRect = async (req, res) => {
+
+//     try {
+//         const reactData = req.body
+//         console.log(reactData)
+//         reactData.map(async (data) => {
+          
+//             const rect = await Rectangle.findOne(
+//                 {
+//                     where: isUUID(String(data.id))? { name: String(data.id) } : { id: data.id }
+//                 }
+//             )
+
+//             if (rect) {
+//                 const updateRect = await Rectangle.update({
+//                     height: data.height,
+//                     width: data.width,
+//                     x: data.x,
+//                     y: data.y,
+//                 }, { where: rect?{id: data.id} : {name: String(data.id) } })
+//                 // return res.status(200).json({ success: true })
+//             } else {
+//                 const createRect = await Rectangle.create({
+//                     height: data.height,
+//                     width: data.width,
+//                     x: data.x,
+//                     y: data.y,
+//                     stroke: data.stroke,
+//                     name: String(data.id),
+//                     template_id: data.template_id
+//                 })
+//             }
+//         })
+//         return res.status(200).json({ success: true })
+
+//     } catch (error) {
+//         return res.status(500).json({
+//             success: false,
+//             error: error.message
+//         })
+//     }
+// }
 const createUpdateRect = async (req, res) => {
-
     try {
-        const reactData = req.body
-        console.log(reactData)
-        reactData.map(async (data) => {
-            const rect = await Rectangle.findOne(
-                {
-                    where: { id: data.id }
-                }
-            )
-            const rect1 = await Rectangle.findOne(
-                {
-                    where: { name: String(data.id) }
-                }
-            )
+        const reactData = req.body;
 
-            if (rect || rect1) {
-                const updateRect = await Rectangle.update({
+        await Promise.all(reactData.map(async (data) => {
+            let rect;
+
+            if (isUUID(String(data.id))) {
+                rect = await Rectangle.findOne({ where: { name: String(data.id) } });
+            } else {
+                rect = await Rectangle.findOne({ where: { id: data.id } });
+            }
+
+            if (rect) {
+                await Rectangle.update({
                     height: data.height,
                     width: data.width,
                     x: data.x,
                     y: data.y,
-                }, { where: rect?{id: data.id} : {name: String(data.id) } })
-                // return res.status(200).json({ success: true })
+                }, { where: { id: rect.id } });
             } else {
-                const createRect = await Rectangle.create({
+                await Rectangle.create({
                     height: data.height,
                     width: data.width,
                     x: data.x,
@@ -73,18 +115,19 @@ const createUpdateRect = async (req, res) => {
                     stroke: data.stroke,
                     name: String(data.id),
                     template_id: data.template_id
-                })
+                });
             }
-        })
-        return res.status(200).json({ success: true })
+        }));
 
+        return res.status(200).json({ success: true });
     } catch (error) {
         return res.status(500).json({
             success: false,
             error: error.message
-        })
+        });
     }
-}
+};
+
 
 const removeRectangle = async (req, res) => {
 
