cfc44443c56427deca4757b2a5a6c59e1f8bbcb2
Gaurav Singh <gauravsingh.info@gmail.com>
2023-11-30 16:06:57 +0530

    fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,11 +7,21 @@ const router = require('./routes')
 const app = express()
 
 app.use(express.json())
-app.use(cors());
+const corsOptions = {
+    origin: 'https://revfin-27hp.vercel.app/', 
+    methods: 'POST, GET, OPTIONS',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+// Handle Preflight (OPTIONS) requests
+app.options('*', cors(corsOptions));
 app.use(router);
 
 // db connection
-db.sync({alter: true}).then(() => {
+db.sync({ alter: true }).then(() => {
     console.log('Database Connected!')
 }).catch((err) => {
     console.log('Error => ', err)
